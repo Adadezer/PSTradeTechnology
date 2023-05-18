dfb37d5e725e2ff8e88a68ebe1871878b98b6440
Adadezer <adadezer@gmail.com>
2023-05-18 17:56:03 -0300

    feat: :sparkles: implementação do context do react

--- tradefrontend/src/App.tsx
+++ tradefrontend/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import TradeProvider from "./context/TradeProvider";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
@@ -8,12 +9,14 @@ import NotFound from "./pages/NotFound";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/404" element={<NotFound />} />
-        <Route path="/" element={<Login />} />
-        <Route path="*" element={<Navigate replace to="/404" />} />
-      </Routes>
+      <TradeProvider>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<Navigate replace to="/404" />} />
+        </Routes>
+      </TradeProvider>
     </>
   );
 }
