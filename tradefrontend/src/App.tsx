d2fa39248c333320c748dd6829db7b04734a8486
Adadezer <adadezer@gmail.com>
2023-05-18 12:37:17 -0300

    feat: :sparkles: adição de rotas react

--- tradefrontend/src/App.tsx
+++ tradefrontend/src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/" element={<Login />} />
+        <Route path="*" element={<Navigate replace to="/404" />} />
+      </Routes>
+    </>
   );
 }
 
