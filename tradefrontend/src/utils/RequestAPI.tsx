19c8d2e192c19bd69b1a7502b8ff80f3ceae9aa0
Adadezer <adadezer@gmail.com>
2023-05-18 18:38:08 -0300

    feat: :sparkles: implementação da logica da pagina de login para home em caso de sucesso, e para not found em caso de erro

--- tradefrontend/src/utils/RequestAPI.tsx
+++ tradefrontend/src/utils/RequestAPI.tsx
@@ -1,17 +1,29 @@
 import axios from "axios";
+import { NavigateFunction } from "react-router-dom";
 
 const apiUrl = "https://v3.football.api-sports.io";
 
-const RequestAPI = async (apiKey: string, endpoint: string) => {
+const RequestAPI = async (
+  apiKey: string,
+  endpoint: string,
+  navigate: NavigateFunction
+) => {
   const config = {
     headers: {
       "x-rapidapi-key": apiKey,
       "x-rapidapi-host": "v3.football.api-sports.io",
     },
   };
+
   try {
     const result = await axios.get(`${apiUrl}/${endpoint}`, config);
     console.log("result:", result);
+
+    if (result.data.errors.length === 0) {
+      navigate("/home");
+    } else {
+      navigate("/404");
+    }
   } catch (error) {
     console.error(error);
   }
